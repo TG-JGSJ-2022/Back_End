1c91e87682097094f97eafef448d41e01316a7eb
Juan Diego Campos Neira <juand.campos@javeriana.edu.co>
2022-04-05 17:09:48 -0500

    Added credentials support

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "1dafafghsdsf5378167ugfdsasdfghj98797781234741arfcshzgwffzgnssaerASXMHMRMDwefsrvs8945)(/%#"
 app.secret_key = "test_secret"
-CORS(app)
+CORS(app, supports_credentials=True)
 login_manager=LoginManager()
 login_manager.init_app(app)
 
